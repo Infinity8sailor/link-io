f55dc319e2ed35e5c6d0b4d5ae8cf7a950f8fc37
vaibhav dasharathe <43951027+Infinity8sailor@users.noreply.github.com>
2020-08-09 11:25:12 +0530

    Update settings.py

--- dJango/settings.py
+++ dJango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '$rmyq@rj-fyva7@+i(@p^fr9ev^&p937-f!ifte07+yfcy@6nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
