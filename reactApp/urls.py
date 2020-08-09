b8c0e063ac775ecbfc1b960c96ea3e838865c659
infinity_sailor <vaibhav.dasharathe18@vit.edu>
2020-08-09 20:31:12 +0530

    static urls setting

--- reactApp/urls.py
+++ reactApp/urls.py
@@ -1,6 +1,9 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', views.index ),
@@ -9,3 +12,4 @@ urlpatterns = [
     path('public/flare-2.json',views.flare),
 
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
