d241918216cf5912a4721b8b01fee3c62d76a4a1
infinity_sailor <vaibhav.dasharathe18@vit.edu>
2020-08-09 10:37:50 +0530

    requirements

--- dJango/settings.py
+++ dJango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$rmyq@rj-fyva7@+i(@p^fr9ev^&p937-f!ifte07+yfcy@6nh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
